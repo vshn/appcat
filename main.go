27af6fef91c09d229f45126b6486726746bea1fb
Gabriel Saratura <gabriel.saratura@vshn.ch>
2023-01-24 15:22:01 +0200

    Remove controller

--- main.go
+++ main.go
@@ -31,8 +31,6 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	appcatcontrollers "apiserver/controllers/appcat"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -78,13 +76,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = (&appcatcontrollers.AppCatReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "AppCat")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {

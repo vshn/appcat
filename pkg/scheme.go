c98df2b693928bab2786e6dd9ad97a8ef69a596d
Simon Beck <simon.beck@vshn.ch>
2023-05-26 08:26:45 +0200

    Add missing stackgres types to scheme

--- pkg/scheme.go
+++ pkg/scheme.go
@@ -2,6 +2,7 @@ package pkg
 
 import (
 	xkube "github.com/crossplane-contrib/provider-kubernetes/apis/object/v1alpha1"
+	stackgresv1 "github.com/vshn/appcat/apis/stackgres/v1"
 	vshnv1 "github.com/vshn/appcat/apis/vshn/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -12,5 +13,6 @@ func SetupScheme() *runtime.Scheme {
 	_ = corev1.SchemeBuilder.AddToScheme(s)
 	_ = xkube.SchemeBuilder.AddToScheme(s)
 	_ = vshnv1.SchemeBuilder.SchemeBuilder.AddToScheme(s)
+	_ = stackgresv1.SchemeBuilder.AddToScheme(s)
 	return s
 }

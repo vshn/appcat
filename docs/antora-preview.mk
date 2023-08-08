1266e996ec864702e1fb5a0e3f4469376a8c81ec
Simon Beck <simon.beck@vshn.ch>
2023-08-08 11:10:23 +0200

    Add docs about PVC resize

--- docs/antora-preview.mk
+++ docs/antora-preview.mk
@@ -1,5 +1,5 @@
 
-antora_preview_version ?= 3.1.2.3
+antora_preview_version ?= 3.1.4
 antora_preview_cmd ?= $(DOCKER_CMD) run --rm --publish 35729:35729 --publish 2020:2020 --volume "${PWD}":/preview/antora ghcr.io/vshn/antora-preview:$(antora_preview_version) --style=vshn
 
 .PHONY: docs-preview

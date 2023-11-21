4c54f6c578d1d9934329e8cd2130c906e1112e87
Simon Beck <simon.beck@vshn.ch>
2023-11-21 11:26:37 +0100

    Add function push to actions

--- package/package.mk
+++ package/package.mk
@@ -9,3 +9,7 @@ install-proxy: package-function
 	docker tag $$id ghcr.io/vshn/appcat:latest-func && \
 	kind load --name kindev docker-image ghcr.io/vshn/appcat:latest-func
 	kubectl apply -f hack/functionproxy
+
+.PHONY
+push-function-package: package-function:
+	go run github.com/crossplane/crossplane/cmd/crank xpkg push -f package/package-function-appcat.xpkg ghcr.io/vshn/appcat:${IMG_TAG}-func --verbose

2cf0cfcf719186c1f6b1cc7a3843b35e111b98d3
Simon Beck <simon.beck@vshn.ch>
2024-03-11 09:31:19 +0100

    Make sure this function never returns on errors

--- pkg/comp-functions/functions/common/instance_namespace.go
+++ pkg/comp-functions/functions/common/instance_namespace.go
@@ -76,7 +76,8 @@ func createInstanceNamespace(ctx context.Context, serviceName, compName, claimNa
 
 	org, err := getOrg(compName, svc)
 	if err != nil {
-		return fmt.Errorf("cannot get claim namespace: %w", err)
+		svc.Log.Info("cannot get claim namespace")
+		svc.AddResult(runtime.NewWarningResult("cannot get claim namespace"))
 	}
 
 	ns := &corev1.Namespace{
@@ -96,7 +97,8 @@ func createInstanceNamespace(ctx context.Context, serviceName, compName, claimNa
 
 	controlNS, ok := svc.Config.Data["controlNamespace"]
 	if !ok {
-		return fmt.Errorf("controlNamespace not specified")
+		svc.Log.Info("no control namespace defined, please make sure that it's defined in the composition inputs")
+		svc.AddResult(runtime.NewWarningResult("no control namespace defined, please make sure that it's defined in the composition inputs"))
 	}
 
 	disabled, err := isBillingDisabled(controlNS, instanceNamespace, compName, svc)

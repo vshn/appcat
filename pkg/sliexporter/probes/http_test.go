673dd38e942148ee2b3af0dae3f3bc98f79e7aac
Simon Beck <simon.beck@vshn.ch>
2024-04-05 10:41:40 +0200

    Add tests for http prober

---
+++ pkg/sliexporter/probes/http_test.go
@@ -0,0 +1,34 @@
+package probes
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHTTP_Probe(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	defer server.Close()
+
+	httpProbe := NewHTTP(server.URL, false, nil, "mockservice", "unittest", "unittest", "unittest", "unittest", false)
+
+	assert.NoError(t, httpProbe.Probe(context.TODO()))
+}
+
+func TestHTTP_Probe_TLS(t *testing.T) {
+	server := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	defer server.Close()
+
+	httpProbe := NewHTTP(server.URL, true, server.Certificate(), "mockservice", "unittest", "unittest", "unittest", "unittest", false)
+
+	assert.NoError(t, httpProbe.Probe(context.TODO()))
+}

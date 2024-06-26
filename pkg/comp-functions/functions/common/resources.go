2f0205872386737451c30bbf215b3c44bdd1ab82
Nicolas Bigler <nicolas.bigler@vshn.ch>
2024-06-26 12:01:29 +0200

    Add comments to new resource calc functions
    Signed-off-by: Nicolas Bigler <nicolas.bigler@vshn.ch>

--- pkg/comp-functions/functions/common/resources.go
+++ pkg/comp-functions/functions/common/resources.go
@@ -14,6 +14,9 @@ type Resources struct {
 	Disk   resource.Quantity
 }
 
+// GetResources will return a `Resources` object with the correctly calculated requests,
+// limits and disk space according to the definitions in the plan as well as the overrides
+// in the claim.
 func GetResources(size *vshnv1.VSHNSizeSpec, plan utils.Resources) (Resources, error) {
 	reqMem := resource.Quantity{}
 	reqCPU := resource.Quantity{}
@@ -73,6 +76,15 @@ func GetResources(size *vshnv1.VSHNSizeSpec, plan utils.Resources) (Resources, e
 	}, nil
 }
 
+// getLimit will compare a given limit and request as well as the limit defined in the plan
+// and return the appropriate limit.
+// The limit is chosen based on the following logic:
+//   - If claim contains a limit but no request: Use the limit from the claim  as the final limit
+//   - If claim contains a limit and a request: Use the higher of both as the final limit
+//     This avoids having higher requests then limits, which is not supported in k8s.
+//   - If claim contains only request: Use the higher of request and limit in the plan as the final limit
+//     this avoids having higher requests then limits, which is not supported in k8s.
+//   - If no limit or request is defined in the claim, use the plans limit as the final limit.
 func getLimit(req, limit, planLimit resource.Quantity) resource.Quantity {
 
 	finalMem := resource.Quantity{}
@@ -89,21 +101,30 @@ func getLimit(req, limit, planLimit resource.Quantity) resource.Quantity {
 	return finalMem
 }
 
+// getRequest will compare a given limit and request as well as the limit defined in the plan
+// and return the appropriate request.
+// This function assumes, that the given limit is already calculated with the `getLimit` function
+// and passed to this function.
+// The request is chosen based on the following logic:
+//   - If no requests is defined in the claim, use the plans request as the final requests.
+//   - If the claim contains a limt and request, use the lower of both as the final request.
+//     This avoids having higher requests then limits, which is not supported in k8s.
 func getRequest(req, limit, planRequests resource.Quantity) resource.Quantity {
 
-	finalMem := req
+	finalReq := req
 
 	if req.IsZero() {
-		finalMem = planRequests
+		finalReq = planRequests
 	}
 
-	if limit.Cmp(finalMem) == -1 {
-		finalMem = limit
+	if limit.Cmp(finalReq) == -1 {
+		finalReq = limit
 	}
 
-	return finalMem
+	return finalReq
 }
 
+// getHigherQuantity will compare the given quantities and return the higher one.
 func getHigherQuantity(a, b resource.Quantity) resource.Quantity {
 
 	if a.Cmp(b) == -1 {

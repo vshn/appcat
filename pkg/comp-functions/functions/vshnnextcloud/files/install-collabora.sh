6897777ac3f90c4a4e741368d9ba76e9e28e4d7c
lukasz.widera@vshn.ch <lukasz.widera@vshn.ch>
2024-11-13 14:56:28 +0100

    improving install-collabora.sh script

--- pkg/comp-functions/functions/vshnnextcloud/files/install-collabora.sh
+++ pkg/comp-functions/functions/vshnnextcloud/files/install-collabora.sh
@@ -6,6 +6,12 @@ COLLABORA_FQDN=$3
 COLLABORA_URL="https://$COLLABORA_FQDN"
 
 if [ "$IS_OPENSHIFT" == "true" ]; then
+
+    while ! /var/www/html/occ status | grep "installed: true" ; do
+        echo "Nextcloud is not ready yet"
+        sleep 5
+    done
+
     #check if collabora is already installed
     /var/www/html/occ app:get richdocuments
     RC=$?
@@ -18,6 +24,14 @@ if [ "$IS_OPENSHIFT" == "true" ]; then
     /var/www/html/occ config:app:set --value "$COLLABORA_URL" richdocuments wopi_url
     /var/www/html/occ richdocuments:activate-config
 else
+    while ! /usr/bin/su -s /bin/sh www-data -c  "/var/www/html/occ status" | grep "installed: true" ; do
+        echo "Nextcloud is not ready yet"
+        sleep 5
+    done
+
+    #check if collabora is already installed
+    /usr/bin/su -s /bin/bash www-data -c  "/var/www/html/occ status" | grep "installed: true" 
+
     /usr/bin/su -s /bin/sh www-data -c  "/var/www/html/occ app:get richdocuments"
     RC=$?
     if [ "$RC" == 0 ]; then

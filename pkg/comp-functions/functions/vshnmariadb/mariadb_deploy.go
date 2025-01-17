b1ecacacd82ad0b47aef30eef13a2d3f8c7d403d
Gabriel Saratura <gabriel.saratura@vshn.net>
2025-01-17 11:44:33 +0100

    Set image registry and repository

--- pkg/comp-functions/functions/vshnmariadb/mariadb_deploy.go
+++ pkg/comp-functions/functions/vshnmariadb/mariadb_deploy.go
@@ -226,6 +226,12 @@ func newValues(ctx context.Context, svc *runtime.ServiceRuntime, comp *vshnv1.VS
 		},
 	}
 
+	if svc.Config.Data["imageRegistry"] != "" {
+		values["image"] = map[string]interface{}{
+			"registry": svc.Config.Data["imageRegistry"],
+		}
+	}
+
 	return values, nil
 }
 

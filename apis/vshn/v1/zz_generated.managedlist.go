515abd25ba1d16d13df7ffe89cff3c1de6c9f577
Łukasz Widera <lukaszwidera1993@gmail.com>
2025-01-28 07:41:49 +0100

    adding Codey base crd,xrd

--- apis/vshn/v1/zz_generated.managedlist.go
+++ apis/vshn/v1/zz_generated.managedlist.go
@@ -4,6 +4,15 @@ package v1
 
 import resource "github.com/crossplane/crossplane-runtime/pkg/resource"
 
+// GetItems of this XCodeyList.
+func (l *XCodeyList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
+
 // GetItems of this XVSHNForgejoList.
 func (l *XVSHNForgejoList) GetItems() []resource.Managed {
 	items := make([]resource.Managed, len(l.Items))

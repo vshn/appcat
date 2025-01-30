6a00784de07b76a5b64f9810d4cb7cf36d542888
Łukasz Widera <lukaszwidera1993@gmail.com>
2025-01-30 14:47:28 +0100

    fixing v1.Conditions generation and population

--- apis/vshn/v1/dbaas_vshn_Forgejo.go
+++ apis/vshn/v1/dbaas_vshn_Forgejo.go
@@ -123,12 +123,6 @@ type VSHNForgejoSizeSpec struct {
 
 // VSHNForgejoStatus reflects the observed state of a VSHNForgejo.
 type VSHNForgejoStatus struct {
-	NamespaceConditions         []Condition `json:"namespaceConditions,omitempty"`
-	SelfSignedIssuerConditions  []Condition `json:"selfSignedIssuerConditions,omitempty"`
-	LocalCAConditions           []Condition `json:"localCAConditions,omitempty"`
-	CaCertificateConditions     []Condition `json:"caCertificateConditions,omitempty"`
-	ServerCertificateConditions []Condition `json:"serverCertificateConditions,omitempty"`
-	ClientCertificateConditions []Condition `json:"clientCertificateConditions,omitempty"`
 	// InstanceNamespace contains the name of the namespace where the instance resides
 	InstanceNamespace string `json:"instanceNamespace,omitempty"`
 	// Schedules keeps track of random generated schedules, is overwriten by

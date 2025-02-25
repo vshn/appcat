463898866c03ba410ebdfca426a61b44df06339a
V. Klopfenstein <13659371+ThisIsntTheWay@users.noreply.github.com>
2025-02-25 10:52:36 +0000

    GetDesiredComposite -> GetObservedComposite

--- pkg/comp-functions/functions/vshnforgejo/backup.go
+++ pkg/comp-functions/functions/vshnforgejo/backup.go
@@ -18,7 +18,7 @@ import (
 var forgejoBackupScript string
 
 func AddBackup(ctx context.Context, comp *vshnv1.VSHNForgejo, svc *runtime.ServiceRuntime) *xfnproto.Result {
-	err := svc.GetDesiredComposite(comp)
+	err := svc.GetObservedComposite(comp)
 	if err != nil {
 		return runtime.NewFatalResult(fmt.Errorf("can't get composite: %w", err))
 	}

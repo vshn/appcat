9b2b60e86d22e40a97d70cc85a133add3598280f
V. Klopfenstein <13659371+ThisIsntTheWay@users.noreply.github.com>
2025-02-26 16:06:27 +0000

    Add maintenace/forgejo.go, extra env vars for maintenance.New()

---
+++ pkg/maintenance/forgejo.go
@@ -0,0 +1,39 @@
+package maintenance
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/go-logr/logr"
+	"github.com/vshn/appcat/v4/pkg/maintenance/helm"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	forgeoURL = "https://codeberg.org/v2/forgejo/forgejo/tags/?page_size=100"
+)
+
+// Forgejo contains all necessary dependencies to successfully run a Forgejo maintenance
+type Forgejo struct {
+	k8sClient  client.Client
+	httpClient *http.Client
+	log        logr.Logger
+}
+
+// NewForgejo returns a new Forgejo object
+func NewForgejo(c client.Client, hc *http.Client) *Forgejo {
+	return &Forgejo{
+		k8sClient:  c,
+		httpClient: hc,
+	}
+}
+
+// DoMaintenance will run minios's maintenance script.
+func (m *Forgejo) DoMaintenance(ctx context.Context) error {
+	m.log = logr.FromContextOrDiscard(ctx).WithValues("type", "forgejo")
+	patcher := helm.NewImagePatcher(m.k8sClient, m.httpClient, m.log)
+
+	valuesPath := helm.NewValuePath("image", "tag")
+
+	return patcher.DoMaintenance(ctx, forgeoURL, valuesPath, helm.SemVerPatchesOnly(true))
+}

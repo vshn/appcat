24edd3102502b3cd11f2d70dd3a4194fad56ee4a
V. Klopfenstein <13659371+ThisIsntTheWay@users.noreply.github.com>
2025-05-12 14:47:03 +0000

    Unit test

--- pkg/comp-functions/functions/vshnforgejo/deploy_test.go
+++ pkg/comp-functions/functions/vshnforgejo/deploy_test.go
@@ -48,6 +48,20 @@ func TestDeployment(t *testing.T) {
 		values := getReleaseValues(t, *release)
 		assert.Equal(t, appName, values["gitea"].(map[string]any)["config"].(map[string]any)["APP_NAME"])
 	})
+
+	t.Run("Ensure_DiskSizeCanBeSet", func(t *testing.T) {
+		const diskSize = "15Gi"
+
+		svc, comp, secretName := bootstrapTest(t)
+		comp.Spec.Parameters.Size.Disk = diskSize
+		assert.NoError(t, addForgejo(context.TODO(), svc, comp, secretName))
+
+		release := &xhelmv1.Release{}
+		assert.NoError(t, svc.GetDesiredComposedResourceByName(release, comp.GetName()))
+
+		values := getReleaseValues(t, *release)
+		assert.Equal(t, diskSize, values["persistence"].(map[string]any)["size"])
+	})
 }
 
 func getReleaseValues(t *testing.T, release xhelmv1.Release) map[string]any {

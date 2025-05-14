0c5c797e5521e1d57b15eb0924a268a4ba0e216a
Marco De Luca <marcodl404@gmail.com>
2025-05-14 10:31:51 +0200

    Add collabora version tests

--- pkg/comp-functions/functions/vshnnextcloud/collabora_test.go
+++ pkg/comp-functions/functions/vshnnextcloud/collabora_test.go
@@ -6,8 +6,9 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/crossplane/function-sdk-go/proto/v1"
+	v1 "github.com/crossplane/function-sdk-go/proto/v1"
 	"github.com/stretchr/testify/assert"
+	appsv1 "k8s.io/api/apps/v1"
 )
 
 func Test_addCollabora(t *testing.T) {
@@ -64,4 +65,49 @@ func Test_addCollabora(t *testing.T) {
 	res = DeployCollabora(ctx, comp, svc)
 
 	assert.True(t, res.Severity == v1.Severity_SEVERITY_NORMAL && res.Message == "Collabora not enabled")
+}
+
+func Test_addCollaboraDefaultVersion(t *testing.T) {
+	svc, comp := getNextcloudComp(t, "vshnnextcloud/01_default.yaml")
+	ctx := context.TODO()
+
+	// Configure Collabora with default version
+	comp.Spec.Parameters.Service.Collabora.Enabled = true
+	comp.Spec.Parameters.Service.Collabora.FQDN = "collabora.example.com"
+
+	assert.Nil(t, DeployNextcloud(ctx, comp, svc))
+
+	result := DeployCollabora(ctx, comp, svc)
+	assert.True(t, result.Severity == v1.Severity_SEVERITY_NORMAL)
+
+	collaboraBaseImage := svc.Config.Data["collabora_image"]
+	defaultImageTag := svc.Config.Data["collabora_image_tag"]
+	expectedImage := fmt.Sprintf("%s:%s", collaboraBaseImage, defaultImageTag)
+
+	sts := &appsv1.StatefulSet{}
+	assert.NoError(t, svc.GetDesiredKubeObject(sts, comp.GetName()+"-collabora-code-sts"))
+	assert.Equal(t, expectedImage, sts.Spec.Template.Spec.Containers[0].Image)
+}
+
+func Test_addCollaboraCustomVersion(t *testing.T) {
+	svc, comp := getNextcloudComp(t, "vshnnextcloud/01_default.yaml")
+	ctx := context.TODO()
+
+	// Configure Collabora with custom version
+	customVersion := "25.04.1.1.1"
+	comp.Spec.Parameters.Service.Collabora.Enabled = true
+	comp.Spec.Parameters.Service.Collabora.FQDN = "collabora.example.com"
+	comp.Spec.Parameters.Service.Collabora.Version = customVersion
+
+	assert.Nil(t, DeployNextcloud(ctx, comp, svc))
+
+	result := DeployCollabora(ctx, comp, svc)
+	assert.True(t, result.Severity == v1.Severity_SEVERITY_NORMAL)
+
+	collaboraBaseImage := svc.Config.Data["collabora_image"]
+	expectedImage := fmt.Sprintf("%s:%s", collaboraBaseImage, customVersion)
+
+	sts := &appsv1.StatefulSet{}
+	assert.NoError(t, svc.GetDesiredKubeObject(sts, comp.GetName()+"-collabora-code-sts"))
+	assert.Equal(t, expectedImage, sts.Spec.Template.Spec.Containers[0].Image)
 }

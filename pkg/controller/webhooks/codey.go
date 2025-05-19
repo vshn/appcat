7e4f7c8d0a1180b009a363f900372162d87dc55e
V. Klopfenstein <13659371+ThisIsntTheWay@users.noreply.github.com>
2025-05-19 09:05:26 +0000

    Webhook: Ignore ACME solver

--- pkg/controller/webhooks/codey.go
+++ pkg/controller/webhooks/codey.go
@@ -130,6 +130,11 @@ func isCodeyFqdnUnique(fqdn, compositeName string, cl client.Client) error {
 	fmt.Printf("IngressList: %s\n", string(o))
 
 	for _, ingress := range ingressList.Items {
+		// Additional filtering to check if the Ingress is actually an ACME solver
+		if _, exists := ingress.Labels["acme.cert-manager.io/http01-solver"]; exists {
+			continue
+		}
+
 		for _, rule := range ingress.Spec.Rules {
 			if rule.Host == fqdn {
 				return field.Invalid(

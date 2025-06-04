96c6559e29b0e35d45f558941a874df76840cdba
Simon Beck <simon.beck@vshn.ch>
2025-06-04 15:11:15 +0200

    Testing new actions

--- ci.mk
+++ ci.mk
@@ -2,8 +2,11 @@
 IMG_TAG ?= latest
 APP_NAME ?= appcat
 ORG ?= vshn
-GHCR_IMG ?= ghcr.io/$(ORG)/$(APP_NAME):$(IMG_TAG)
+IMG_REPO ?= ghcr.io
+IMG ?= $(IMG_REPO)/$(ORG)/$(APP_NAME):$(IMG_TAG)
 DOCKER_CMD ?= docker
+
+
 
 # For alpine image it is required the following env before building the application
 DOCKER_IMAGE_GOOS = linux
@@ -13,36 +16,46 @@ DOCKER_IMAGE_GOARCH = amd64
 docker-build:
 	env CGO_ENABLED=0 GOOS=$(DOCKER_IMAGE_GOOS) GOARCH=$(DOCKER_IMAGE_GOARCH) \
 		go build -o ${BIN_FILENAME}
-	docker build --platform $(DOCKER_IMAGE_GOOS)/$(DOCKER_IMAGE_GOARCH) -t ${GHCR_IMG} .
+	docker build --platform $(DOCKER_IMAGE_GOOS)/$(DOCKER_IMAGE_GOARCH) -t ${IMG} .
 
 .PHONY: docker-build-branchtag
-IMG_TAG =  $(shell git rev-parse --abbrev-ref HEAD | sed 's/\//_/g')
+docker-build-branchtag: export IMG_TAG=$(shell git rev-parse --abbrev-ref HEAD | sed 's/\//_/g')
 docker-build-branchtag: docker-build ## Build docker image with current branch name
 
 .PHONY: docker-push
 docker-push: docker-build ## Push docker image with the manager.
-	docker push ${GHCR_IMG}
+	docker push ${IMG}
 
 .PHONY: docker-push-branchtag
-IMG_TAG =  $(shell git rev-parse --abbrev-ref HEAD | sed 's/\//_/g')
+docker-push-branchtag: export IMG_TAG=$(shell git rev-parse --abbrev-ref HEAD | sed 's/\//_/g')
 docker-push-branchtag: docker-build-branchtag docker-push ## Push docker image with current branch name
 
-.PHONY: function-build
-function-build: docker-build
+.PHONY: package-build
+package-build: docker-build
 	rm -f package/*.xpkg
-	go run github.com/crossplane/crossplane/cmd/crank@v1.16.0 xpkg build -f package --verbose --embed-runtime-image=${GHCR_IMG} -o package/package.xpkg
+	go run github.com/crossplane/crossplane/cmd/crank@v1.16.0 xpkg build -f package --verbose --embed-runtime-image=${IMG} -o package/package.xpkg
 
-.PHONY: function-push-package
-function-push-package: function-build
-	go run github.com/crossplane/crossplane/cmd/crank@v1.16.0 xpkg push -f package/package.xpkg ${GHCR_IMG}-func --verbose
+.PHONY: package-push
+package-push: package-build
+	go run github.com/crossplane/crossplane/cmd/crank@v1.16.0 xpkg push -f package/package.xpkg ${IMG}-func --verbose
 
-.PHONY: function-build-branchtag
-IMG_TAG =  $(shell git rev-parse --abbrev-ref HEAD | sed 's/\//_/g')
-function-build-branchtag: docker-build-branchtag
-	rm -f package/*.xpkg
-	go run github.com/crossplane/crossplane/cmd/crank@v1.16.0 xpkg build -f package --verbose --embed-runtime-image=${GHCR_IMG} -o package/package.xpkg
+.PHONY: package-build-branchtag
+package-build-branchtag: export IMG_TAG=$(shell git rev-parse --abbrev-ref HEAD | sed 's/\//_/g')
+package-build-branchtag: docker-build-branchtag package-build
 
-.PHONY: function-push-package-branchtag
-IMG_TAG =  $(shell git rev-parse --abbrev-ref HEAD | sed 's/\//_/g')
-function-push-package-branchtag: function-build-branchtag
-	go run github.com/crossplane/crossplane/cmd/crank@v1.16.0 xpkg push -f package/package.xpkg ${GHCR_IMG}-func --verbose
+.PHONY: package-push-package-branchtag
+package-push-branchtag: export IMG_TAG=$(shell git rev-parse --abbrev-ref HEAD | sed 's/\//_/g')
+package-push-branchtag: package-build-branchtag package-push
+
+.PHONY: docker-build-local
+docker-build-local: export IMG_REPO=localhost:5000
+docker-build-local:
+	$(MAKE) docker-build
+
+.PHONY: package-build-local
+package-build-local: export IMG_REPO=localhost:5000
+package-build-local: docker-build-local package-build
+
+.PHONY: package-push-local
+package-push-local: export IMG_REPO=localhost:5000
+package-push-local: package-build-local package-push

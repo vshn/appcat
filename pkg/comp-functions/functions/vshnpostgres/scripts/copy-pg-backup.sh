c586c2fc47ddca9988b24858f02f68ec1edc05eb
Nicolas Bigler <nicolas.bigler@vshn.ch>
2025-06-06 08:37:43 +0000

    Add automated restore for Keycloak
    Signed-off-by: Nicolas Bigler <nicolas.bigler@vshn.ch>

--- pkg/comp-functions/functions/vshnpostgres/scripts/copy-pg-backup.sh
+++ pkg/comp-functions/functions/vshnpostgres/scripts/copy-pg-backup.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-set -e
+set -xe
 
-xrdname=$(kubectl -n "${CLAIM_NAMESPACE}" get vshnpostgresqls "${CLAIM_NAME}" -ojson | jq -r '.spec.resourceRef.name')
+xrdtype="${CLAIM_TYPE}"
 
-source_namespace=$(kubectl -n "${CLAIM_NAMESPACE}" get vshnpostgresqls "${CLAIM_NAME}" -ojson | jq -r '.status.instanceNamespace')
+if [ "${xrdtype}" != "" ]
+then
+    xrdservicename=$(kubectl -n "${CLAIM_NAMESPACE}" get "${xrdtype#?}" "${CLAIM_NAME}" -ojson | jq -r '.spec.resourceRef.name')
+    xrdname=$(kubectl -n "${CLAIM_NAMESPACE}" get "${xrdtype}" "${xrdservicename}" -ojson | jq -r '.spec.resourceRefs | map(select(.kind == "XVSHNPostgreSQL")) | .[].name')
+else
+    xrdname=$(kubectl -n "${CLAIM_NAMESPACE}" get vshnpostgresqls "${CLAIM_NAME}" -ojson | jq -r '.spec.resourceRef.name')
+fi
+source_namespace=$(kubectl get xvshnpostgresqls "${xrdname}" -ojson | jq -r '.status.instanceNamespace')
 
 echo "copy secret"
 kubectl -n "${source_namespace}" get secret "pgbucket-${xrdname}" -ojson | jq 'del(.metadata.namespace) | del(.metadata.ownerReferences)' | kubectl -n "${TARGET_NAMESPACE}" apply -f -

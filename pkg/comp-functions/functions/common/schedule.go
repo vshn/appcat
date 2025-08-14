67350c154dc907ff57ce904be73605909c47373f
Gabriel Saratura <gabriel.saratura@vshn.net>
2025-08-14 13:22:18 +0200

    Expand maintenance window

--- pkg/comp-functions/functions/common/schedule.go
+++ pkg/comp-functions/functions/common/schedule.go
@@ -26,12 +26,26 @@ type MaintenanceScheduler interface {
 	GetMaintenanceTimeOfDay() *v1.TimeOfDay
 }
 
-// SetRandomSchedules initializes the backup and maintenance schedules  if the user did not explicitly provide a schedule.
-// The maintenance will be set to a random time on Tuesday night between 21:00 and 5:00, and the backup schedule will be set to once a day between 20:00 and 4:00.
+// SetRandomSchedules initializes the backup and maintenance schedules if the user did not explicitly provide a schedule.
+// The maintenance will be set to a random time on a random day (Sunday-Friday) between 21:00 and 5:00,
+// with the exception that Sunday maintenance only runs after 21:00 (not in the early morning hours).
+// The backup schedule will be set to once a day between 20:00 and 4:00.
 // If neither maintenance nor backup is set, the function will make sure that there will be backup scheduled one hour before the maintenance.
 func SetRandomSchedules(backup BackupScheduler, maintenance MaintenanceScheduler) {
 	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+	availableDays := []string{"sunday", "monday", "tuesday", "wednesday", "thursday", "friday"}
+	selectedDay := availableDays[rng.Intn(len(availableDays))]
 	maintTime := time.Unix(maitenanceWindowStart.Unix()+rng.Int63n(maitenanceWindowRange), 0).In(time.UTC)
+
+	// Special handling for Sunday: only allow times after 21:00 (not early morning)
+	if selectedDay == "sunday" && maintTime.Hour() < 21 {
+		// If time is in early morning (0-5), shift to evening (21-23)
+		eveningStart := time.Date(1970, 1, 1, 21, 0, 0, 0, time.UTC)
+		eveningEnd := time.Date(1970, 1, 1, 23, 59, 59, 0, time.UTC)
+		eveningRange := eveningEnd.Unix() - eveningStart.Unix()
+		maintTime = time.Unix(eveningStart.Unix()+rng.Int63n(eveningRange), 0).In(time.UTC)
+	}
+
 	backupTime := maintTime.Add(-1 * time.Hour).In(time.UTC)
 
 	if backup.GetBackupSchedule() == "" {
@@ -45,10 +59,6 @@ func SetRandomSchedules(backup BackupScheduler, maintenance MaintenanceScheduler
 	}
 
 	if maintenance.GetMaintenanceDayOfWeek() == "" {
-		day := "tuesday"
-		if maintTime.Day() > 1 {
-			day = "wednesday"
-		}
-		maintenance.SetMaintenanceDayOfWeek(day)
+		maintenance.SetMaintenanceDayOfWeek(selectedDay)
 	}
 }

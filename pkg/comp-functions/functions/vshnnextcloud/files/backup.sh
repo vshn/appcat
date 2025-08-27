8d117accce60b25f69e51e5cf98745df4451cbf8
Mike Ditton <mike.ditton@vshn.net>
2025-08-27 13:48:02 +0200

    Fixed permission denied error for nextcloud backups

--- pkg/comp-functions/functions/vshnnextcloud/files/backup.sh
+++ pkg/comp-functions/functions/vshnnextcloud/files/backup.sh
@@ -2,19 +2,28 @@
 
 set -e
 
-# We need to run the occ command as the www-data user, but there's no sudo by default
-apt update 1>&2 && apt install sudo -y 1>&2
+# Determine if we need to switch users to run occ
+OCC_OWNER=$(stat -c '%U' /var/www/html/occ 2>/dev/null || echo "www-data")
+CURRENT_USER=$(whoami)
+
+run_occ() {
+  if [ "$CURRENT_USER" = "$OCC_OWNER" ]; then
+    /var/www/html/occ "$@"
+  else
+    runuser -u "$OCC_OWNER" -- /var/www/html/occ "$@"
+  fi
+}
 
 function disableMaintenance {
   >&2 echo "Disabling maintenance"
-  sudo -u www-data /var/www/html/occ maintenance:mode --off 1>&2
+  run_occ maintenance:mode --off 1>&2
 }
 
 if [ "$SKIP_MAINTENANCE" = false ]; then
 
   trap disableMaintenance EXIT
 
-  sudo -u www-data /var/www/html/occ maintenance:mode --on 1>&2
+  run_occ maintenance:mode --on 1>&2
 fi
 
 tar -cf - /var/www

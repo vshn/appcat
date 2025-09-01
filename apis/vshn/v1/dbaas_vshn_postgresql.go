1c2ff18f04cefb6f46ad6aa5dcc2ee54923ab457
V. Klopfenstein <13659371+ThisIsntTheWay@users.noreply.github.com>
2025-09-01 09:25:03 +0000

    Move comment so that it doesn't show up in IntelliSense

--- apis/vshn/v1/dbaas_vshn_postgresql.go
+++ apis/vshn/v1/dbaas_vshn_postgresql.go
@@ -81,10 +81,11 @@ type VSHNPostgreSQLParameters struct {
 	// Out of all Postgres servers, one is elected as the primary, the rest remain as read-only replicas.
 	Instances int `json:"instances,omitempty"`
 
+	// +kubebuilder:validation:XValidation:rule="self == oldSelf",message="The method of deployment cannot be changed after creation"
+
 	// If enabled, provisions this instance using the CNPG operator instead of StackGres.
 	// Certain limitations apply when deploying using CPNG.
 	// Must be set when creating an instance and cannot be changed afterwards.
-	// +kubebuilder:validation:XValidation:rule="self == oldSelf",message="The method of deployment cannot be changed after creation"
 	UseCNPG bool `json:"useCnpg,omitempty"`
 
 	// This section allows to configure Postgres replication mode and HA roles groups.

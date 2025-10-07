53255063c539b2aaaa9335d1d64c1930fd37ca09
Norbert Gruszka <norbert@bespinian.io>
2025-10-07 10:51:21 +0200

    feat(): generate OpenBao API

--- apis/vshn/v1/zz_generated.managed.go
+++ apis/vshn/v1/zz_generated.managed.go
@@ -364,6 +364,66 @@ func (mg *XVSHNNextcloud) SetWriteConnectionSecretToReference(r *xpv1.SecretRefe
 	mg.Spec.WriteConnectionSecretToReference = r
 }
 
+// GetCondition of this XVSHNOpenBao.
+func (mg *XVSHNOpenBao) GetCondition(ct xpv1.ConditionType) xpv1.Condition {
+	return mg.Status.GetCondition(ct)
+}
+
+// GetDeletionPolicy of this XVSHNOpenBao.
+func (mg *XVSHNOpenBao) GetDeletionPolicy() xpv1.DeletionPolicy {
+	return mg.Spec.DeletionPolicy
+}
+
+// GetManagementPolicies of this XVSHNOpenBao.
+func (mg *XVSHNOpenBao) GetManagementPolicies() xpv1.ManagementPolicies {
+	return mg.Spec.ManagementPolicies
+}
+
+// GetProviderConfigReference of this XVSHNOpenBao.
+func (mg *XVSHNOpenBao) GetProviderConfigReference() *xpv1.Reference {
+	return mg.Spec.ProviderConfigReference
+}
+
+// GetPublishConnectionDetailsTo of this XVSHNOpenBao.
+func (mg *XVSHNOpenBao) GetPublishConnectionDetailsTo() *xpv1.PublishConnectionDetailsTo {
+	return mg.Spec.PublishConnectionDetailsTo
+}
+
+// GetWriteConnectionSecretToReference of this XVSHNOpenBao.
+func (mg *XVSHNOpenBao) GetWriteConnectionSecretToReference() *xpv1.SecretReference {
+	return mg.Spec.WriteConnectionSecretToReference
+}
+
+// SetConditions of this XVSHNOpenBao.
+func (mg *XVSHNOpenBao) SetConditions(c ...xpv1.Condition) {
+	mg.Status.SetConditions(c...)
+}
+
+// SetDeletionPolicy of this XVSHNOpenBao.
+func (mg *XVSHNOpenBao) SetDeletionPolicy(r xpv1.DeletionPolicy) {
+	mg.Spec.DeletionPolicy = r
+}
+
+// SetManagementPolicies of this XVSHNOpenBao.
+func (mg *XVSHNOpenBao) SetManagementPolicies(r xpv1.ManagementPolicies) {
+	mg.Spec.ManagementPolicies = r
+}
+
+// SetProviderConfigReference of this XVSHNOpenBao.
+func (mg *XVSHNOpenBao) SetProviderConfigReference(r *xpv1.Reference) {
+	mg.Spec.ProviderConfigReference = r
+}
+
+// SetPublishConnectionDetailsTo of this XVSHNOpenBao.
+func (mg *XVSHNOpenBao) SetPublishConnectionDetailsTo(r *xpv1.PublishConnectionDetailsTo) {
+	mg.Spec.PublishConnectionDetailsTo = r
+}
+
+// SetWriteConnectionSecretToReference of this XVSHNOpenBao.
+func (mg *XVSHNOpenBao) SetWriteConnectionSecretToReference(r *xpv1.SecretReference) {
+	mg.Spec.WriteConnectionSecretToReference = r
+}
+
 // GetCondition of this XVSHNPostgreSQL.
 func (mg *XVSHNPostgreSQL) GetCondition(ct xpv1.ConditionType) xpv1.Condition {
 	return mg.Status.GetCondition(ct)

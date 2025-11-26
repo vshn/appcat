088c8edc81a74f8f440f808d10afeba84f834e14
Marco De Luca <marcodl404@gmail.com>
2025-11-26 11:02:45 +0000

    Enhance Keycloak configuration handling and resource copying
    - Updated handleCustomConfig to include envFrom processing for Secrets and ConfigMaps
    - Modified AddMaintenanceJob to prioritize desired composite retrieval
    - Adjusted CopyKubeResource to allow deletion

--- pkg/comp-functions/functions/vshnkeycloak/maintenance.go
+++ pkg/comp-functions/functions/vshnkeycloak/maintenance.go
@@ -14,8 +14,11 @@ import (
 
 // AddMaintenanceJob will add a job to do the maintenance for the instance
 func AddMaintenanceJob(ctx context.Context, comp *vshnv1.VSHNKeycloak, svc *runtime.ServiceRuntime) *xfnproto.Result {
-	if err := svc.GetObservedComposite(comp); err != nil {
-		return runtime.NewFatalResult(fmt.Errorf("can't get desired composite: %w", err))
+	// Try first to get from desired and if not found fall back to observed
+	if err := svc.GetDesiredComposite(comp); err != nil {
+		if err := svc.GetObservedComposite(comp); err != nil {
+			return runtime.NewFatalResult(fmt.Errorf("can't get composite: %w", err))
+		}
 	}
 
 	maintTime := common.SetRandomMaintenanceSchedule(comp)

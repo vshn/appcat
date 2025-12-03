7c466fe4902252752e69e08cd1ffa80f5e1ec374
Simon Hofer <simon.hofer@spoud.io>
2025-12-03 14:28:05 +0100

    add kafkastrimzi

---
+++ pkg/comp-functions/functions/vshnkafkastrimzi/billing.go
@@ -0,0 +1,35 @@
+package vshnkafkastrimzi
+
+import (
+	"context"
+	"fmt"
+
+	xfnproto "github.com/crossplane/function-sdk-go/proto/v1"
+	v1 "github.com/vshn/appcat/v4/apis/vshn/v1"
+	"github.com/vshn/appcat/v4/pkg/comp-functions/functions/common"
+	"github.com/vshn/appcat/v4/pkg/comp-functions/runtime"
+)
+
+// AddBilling enables billing for this service
+// It runs both the legacy Prometheus-based billing and the BillingService CR-based billing
+func AddBilling(ctx context.Context, comp *v1.VSHNKafkaStrimzi, svc *runtime.ServiceRuntime) *xfnproto.Result {
+	err := svc.GetObservedComposite(comp)
+	if err != nil {
+		return runtime.NewFatalResult(fmt.Errorf("can't get composite: %w", err))
+	}
+
+	// Keep the existing Prometheus-based billing
+	prometheusResult := common.CreateBillingRecord(ctx, svc, comp)
+	if prometheusResult != nil && prometheusResult.Severity != xfnproto.Severity_SEVERITY_NORMAL {
+		return prometheusResult
+	}
+
+	// Add BillingService CR-based billing
+	billingServiceResult := common.CreateOrUpdateBillingService(ctx, svc, comp)
+
+	if billingServiceResult != nil && billingServiceResult.Severity != xfnproto.Severity_SEVERITY_NORMAL {
+		return billingServiceResult
+	}
+
+	return runtime.NewNormalResult(fmt.Sprintf("Billing enabled for instance %s", comp.GetName()))
+}

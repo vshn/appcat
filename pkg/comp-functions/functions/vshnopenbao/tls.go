493bfbfdb8a898c7f7f294a6dc665ef8e5e20de8
Norbert Gruszka <norbert@bespinian.io>
2026-01-06 10:21:12 +0100

    Bootstrap TLS Certificates for OpenBao

---
+++ pkg/comp-functions/functions/vshnopenbao/tls.go
@@ -0,0 +1,244 @@
+package vshnopenbao
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	cmv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	certmgrv1 "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
+	xkube "github.com/vshn/appcat/v4/apis/kubernetes/v1alpha2"
+	vshnv1 "github.com/vshn/appcat/v4/apis/vshn/v1"
+	"github.com/vshn/appcat/v4/pkg/comp-functions/functions/common"
+	"github.com/vshn/appcat/v4/pkg/comp-functions/runtime"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func enableOpenBaoTLSSupport(ctx context.Context, comp *vshnv1.VSHNOpenBao, svc *runtime.ServiceRuntime) error {
+	serviceName := comp.GetName()
+	ns := comp.GetInstanceNamespace()
+
+	selfSignedIssuerName := serviceName + "-selfsigned-issuer"
+
+	rootCAName := serviceName + "-ca"
+	rootCASecretName := serviceName + "-ca-tls"
+	rootCAIssuerName := serviceName + "-ca-issuer"
+
+	openBaoServerCertName := serviceName + "-server"
+	openBaoServerCertSecretName := serviceName + "-server-tls"
+
+	selfSignedIssuerOpts := &common.TLSOptions{
+		IssuerOptions: []common.IssuerOption{
+			withIssuerOfTypeSelfSigned(),
+		},
+	}
+	selfSignedIssuer := createIssuer(ns, selfSignedIssuerName, selfSignedIssuerOpts)
+	err := svc.SetDesiredKubeObject(selfSignedIssuer, selfSignedIssuerName, selfSignedIssuerOpts.KubeOptions...)
+	if err != nil {
+		err = fmt.Errorf("cannot create Self Signed Issuer for OpenBao %w", err)
+		return err
+	}
+
+	rootCAOpts := &common.TLSOptions{
+		CertOptions: []common.CertOptions{
+			withCertificateSecretName(rootCASecretName),
+			withCertificateDuration(time.Duration(87600 * time.Hour)),
+			withCertificateRenewBefore(time.Duration(2400 * time.Hour)),
+			withCertificateIssuerRef(selfSignedIssuerName),
+			withCertificateIsCA(true),
+		},
+		KubeOptions: []runtime.KubeObjectOption{},
+	}
+	rootCA := createCertificate(ns, rootCAName, rootCAOpts)
+	err = svc.SetDesiredKubeObject(rootCA, rootCAName, rootCAOpts.KubeOptions...)
+	if err != nil {
+		err = fmt.Errorf("cannot create Root CA with Self Signed Issuer for OpenBao %w", err)
+		return err
+	}
+
+	rootCAIssuerOpts := &common.TLSOptions{
+		IssuerOptions: []common.IssuerOption{
+			withIssuerOfTypeCA(rootCASecretName),
+		},
+	}
+	rootCAIssuer := createIssuer(ns, rootCAIssuerName, rootCAIssuerOpts)
+	err = svc.SetDesiredKubeObject(rootCAIssuer, rootCAIssuerName, rootCAIssuerOpts.KubeOptions...)
+	if err != nil {
+		err = fmt.Errorf("cannot create Root CA Issuer for OpenBao %w", err)
+		return err
+	}
+
+	openBaoServerCertOpts := &common.TLSOptions{
+		CertOptions: []common.CertOptions{
+			withCertificateSecretName(openBaoServerCertSecretName),
+			withCertificateDuration(time.Duration(87600 * time.Hour)),
+			withCertificateRenewBefore(time.Duration(2400 * time.Hour)),
+			withCertificateIssuerRef(rootCAIssuerName),
+			withCertificateDNSName(serviceName + "." + ns + ".svc.cluster.local"),
+			withCertificateDNSName(serviceName + "." + ns + ".svc"),
+			withCertificateUsages([]cmv1.KeyUsage{"server auth", "client auth"}),
+		},
+		KubeOptions: []runtime.KubeObjectOption{
+			runtime.KubeOptionAddConnectionDetails(ns,
+				xkube.ConnectionDetail{
+					ObjectReference: corev1.ObjectReference{
+						APIVersion: "v1",
+						Kind:       "Secret",
+						Namespace:  ns,
+						Name:       openBaoServerCertSecretName,
+						FieldPath:  "data[ca.crt]",
+					},
+					ToConnectionSecretKey: "ca.crt",
+				},
+				xkube.ConnectionDetail{
+					ObjectReference: corev1.ObjectReference{
+						APIVersion: "v1",
+						Kind:       "Secret",
+						Namespace:  ns,
+						Name:       openBaoServerCertSecretName,
+						FieldPath:  "data[tls.crt]",
+					},
+					ToConnectionSecretKey: "tls.crt",
+				},
+				xkube.ConnectionDetail{
+					ObjectReference: corev1.ObjectReference{
+						APIVersion: "v1",
+						Kind:       "Secret",
+						Namespace:  ns,
+						Name:       openBaoServerCertSecretName,
+						FieldPath:  "data[tls.key]",
+					},
+					ToConnectionSecretKey: "tls.key",
+				},
+			),
+		},
+	}
+	openBaoServerCert := createCertificate(ns, openBaoServerCertName, openBaoServerCertOpts)
+	err = svc.SetDesiredKubeObject(openBaoServerCert, openBaoServerCertName, openBaoServerCertOpts.KubeOptions...)
+	if err != nil {
+		err = fmt.Errorf("cannot create Server Certificate for OpenBao %w", err)
+		return err
+	}
+
+	return nil
+}
+
+func withIssuerOfTypeCA(secretName string) common.IssuerOption {
+	return func(issuer *cmv1.Issuer) {
+		issuer.Spec.CA = &cmv1.CAIssuer{
+			SecretName: secretName,
+		}
+	}
+}
+
+func withIssuerOfTypeSelfSigned() common.IssuerOption {
+	return func(issuer *cmv1.Issuer) {
+		issuer.Spec.SelfSigned = &cmv1.SelfSignedIssuer{
+			CRLDistributionPoints: []string{},
+		}
+	}
+}
+
+func withCertificateSecretName(secretName string) common.CertOptions {
+	return func(cert *cmv1.Certificate) {
+		cert.Spec.SecretName = secretName
+	}
+}
+
+func withCertificateDNSName(dnsName string) common.CertOptions {
+	return func(cert *cmv1.Certificate) {
+		if cert.Spec.DNSNames == nil {
+			cert.Spec.DNSNames = []string{}
+		}
+		cert.Spec.DNSNames = append(cert.Spec.DNSNames, dnsName)
+	}
+}
+
+func withCertificateIsCA(isCA bool) common.CertOptions {
+	return func(cert *cmv1.Certificate) {
+		cert.Spec.IsCA = isCA
+	}
+}
+
+func withCertificateIssuerRef(issuerName string) common.CertOptions {
+	return func(cert *cmv1.Certificate) {
+		cert.Spec.IssuerRef = certmgrv1.ObjectReference{
+			Name:  issuerName,
+			Kind:  "Issuer",
+			Group: "cert-manager.io",
+		}
+	}
+}
+
+func withCertificateDuration(time time.Duration) common.CertOptions {
+	return func(cert *cmv1.Certificate) {
+		cert.Spec.Duration = &metav1.Duration{
+			Duration: time,
+		}
+	}
+}
+
+func withCertificateRenewBefore(time time.Duration) common.CertOptions {
+	return func(cert *cmv1.Certificate) {
+		cert.Spec.RenewBefore = &metav1.Duration{
+			Duration: time,
+		}
+	}
+}
+
+func withCertificateUsages(usages []cmv1.KeyUsage) common.CertOptions {
+	return func(cert *cmv1.Certificate) {
+		cert.Spec.Usages = usages
+	}
+}
+
+func createIssuer(namespace string, issuerName string, opts *common.TLSOptions) *cmv1.Issuer {
+	resource := &cmv1.Issuer{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      issuerName,
+			Namespace: namespace,
+		},
+		Spec: cmv1.IssuerSpec{
+			IssuerConfig: cmv1.IssuerConfig{},
+		},
+	}
+
+	if opts.IssuerOptions != nil {
+		for _, opt := range opts.IssuerOptions {
+			opt(resource)
+		}
+	}
+
+	return resource
+}
+
+func createCertificate(namespace string, certName string, opts *common.TLSOptions) *cmv1.Certificate {
+	resource := &cmv1.Certificate{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      certName,
+			Namespace: namespace,
+		},
+		Spec: cmv1.CertificateSpec{
+			PrivateKey: &cmv1.CertificatePrivateKey{
+				Algorithm: cmv1.RSAKeyAlgorithm,
+				Encoding:  cmv1.PKCS1,
+				Size:      4096,
+			},
+			CommonName: certName + "-ca",
+			Subject: &cmv1.X509Subject{
+				Organizations: []string{
+					"vshn-appcat-ca",
+				},
+			},
+		},
+	}
+
+	if opts.CertOptions != nil {
+		for _, opt := range opts.CertOptions {
+			opt(resource)
+		}
+	}
+
+	return resource
+}
---
+++ pkg/comp-functions/functions/vshnopenbao/tls_test.go
@@ -0,0 +1,312 @@
+package vshnopenbao
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	cmv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	xkube "github.com/vshn/appcat/v4/apis/kubernetes/v1alpha2"
+	vshnv1 "github.com/vshn/appcat/v4/apis/vshn/v1"
+	"github.com/vshn/appcat/v4/pkg/comp-functions/functions/common"
+	"github.com/vshn/appcat/v4/pkg/comp-functions/functions/commontest"
+)
+
+func TestEnableOpenBaoTLSSupport(t *testing.T) {
+	svc := commontest.LoadRuntimeFromFile(t, "vshnopenbao/deploy/01_default.yaml")
+
+	comp := &vshnv1.VSHNOpenBao{}
+	err := svc.GetObservedComposite(comp)
+	assert.NoError(t, err)
+
+	ctx := context.TODO()
+
+	err = enableOpenBaoTLSSupport(ctx, comp, svc)
+	assert.NoError(t, err)
+
+	serviceName := comp.GetName()
+	ns := comp.GetInstanceNamespace()
+
+	// Test self-signed issuer
+	selfSignedIssuer := &cmv1.Issuer{}
+	err = svc.GetDesiredKubeObject(selfSignedIssuer, serviceName+"-selfsigned-issuer")
+	assert.NoError(t, err)
+	assert.NotNil(t, selfSignedIssuer.Spec.SelfSigned)
+	assert.Equal(t, ns, selfSignedIssuer.Namespace)
+
+	// Test root CA certificate
+	rootCA := &cmv1.Certificate{}
+	err = svc.GetDesiredKubeObject(rootCA, serviceName+"-ca")
+	assert.NoError(t, err)
+	assert.Equal(t, serviceName+"-ca-tls", rootCA.Spec.SecretName)
+	assert.True(t, rootCA.Spec.IsCA)
+	assert.Equal(t, serviceName+"-selfsigned-issuer", rootCA.Spec.IssuerRef.Name)
+	assert.Equal(t, "Issuer", rootCA.Spec.IssuerRef.Kind)
+	assert.Equal(t, time.Duration(87600*time.Hour), rootCA.Spec.Duration.Duration)
+	assert.Equal(t, time.Duration(2400*time.Hour), rootCA.Spec.RenewBefore.Duration)
+
+	// Test root CA issuer
+	rootCAIssuer := &cmv1.Issuer{}
+	err = svc.GetDesiredKubeObject(rootCAIssuer, serviceName+"-ca-issuer")
+	assert.NoError(t, err)
+	assert.NotNil(t, rootCAIssuer.Spec.CA)
+	assert.Equal(t, serviceName+"-ca-tls", rootCAIssuer.Spec.CA.SecretName)
+
+	// Test OpenBao server certificate
+	serverCert := &cmv1.Certificate{}
+	err = svc.GetDesiredKubeObject(serverCert, serviceName+"-server")
+	assert.NoError(t, err)
+	assert.Equal(t, serviceName+"-server-tls", serverCert.Spec.SecretName)
+	assert.Equal(t, serviceName+"-ca-issuer", serverCert.Spec.IssuerRef.Name)
+	assert.Contains(t, serverCert.Spec.DNSNames, serviceName+"."+ns+".svc.cluster.local")
+	assert.Contains(t, serverCert.Spec.DNSNames, serviceName+"."+ns+".svc")
+	assert.Contains(t, serverCert.Spec.Usages, cmv1.KeyUsage("server auth"))
+	assert.Contains(t, serverCert.Spec.Usages, cmv1.KeyUsage("client auth"))
+
+	// Test connection details
+	obj := &xkube.Object{}
+	err = svc.GetDesiredComposedResourceByName(obj, serviceName+"-server")
+	assert.NoError(t, err)
+	require.NotNil(t, obj.Spec.ConnectionDetails)
+	assert.Len(t, obj.Spec.ConnectionDetails, 3)
+
+	// Verify ca.crt connection detail
+	caCrtDetail := findConnectionDetail(obj.Spec.ConnectionDetails, "ca.crt")
+	assert.NotNil(t, caCrtDetail)
+	assert.Equal(t, serviceName+"-server-tls", caCrtDetail.ObjectReference.Name)
+	assert.Equal(t, "data[ca.crt]", caCrtDetail.ObjectReference.FieldPath)
+
+	// Verify tls.crt connection detail
+	tlsCrtDetail := findConnectionDetail(obj.Spec.ConnectionDetails, "tls.crt")
+	assert.NotNil(t, tlsCrtDetail)
+	assert.Equal(t, serviceName+"-server-tls", tlsCrtDetail.ObjectReference.Name)
+	assert.Equal(t, "data[tls.crt]", tlsCrtDetail.ObjectReference.FieldPath)
+
+	// Verify tls.key connection detail
+	tlsKeyDetail := findConnectionDetail(obj.Spec.ConnectionDetails, "tls.key")
+	assert.NotNil(t, tlsKeyDetail)
+	assert.Equal(t, serviceName+"-server-tls", tlsKeyDetail.ObjectReference.Name)
+	assert.Equal(t, "data[tls.key]", tlsKeyDetail.ObjectReference.FieldPath)
+}
+
+func TestCreateIssuer(t *testing.T) {
+	svc := commontest.LoadRuntimeFromFile(t, "vshnopenbao/deploy/01_default.yaml")
+
+	comp := &vshnv1.VSHNOpenBao{}
+	err := svc.GetObservedComposite(comp)
+	assert.NoError(t, err)
+
+	ns := comp.GetInstanceNamespace()
+
+	tests := []struct {
+		name         string
+		issuerName   string
+		setupOpts    func() *common.TLSOptions
+		validateFunc func(*testing.T, *cmv1.Issuer)
+	}{
+		{
+			name:       "self-signed issuer",
+			issuerName: "test-selfsigned",
+			setupOpts: func() *common.TLSOptions {
+				return &common.TLSOptions{
+					IssuerOptions: []common.IssuerOption{
+						withIssuerOfTypeSelfSigned(),
+					},
+				}
+			},
+			validateFunc: func(t *testing.T, issuer *cmv1.Issuer) {
+				assert.NotNil(t, issuer.Spec.SelfSigned)
+				assert.Equal(t, []string{}, issuer.Spec.SelfSigned.CRLDistributionPoints)
+			},
+		},
+		{
+			name:       "CA issuer",
+			issuerName: "test-ca",
+			setupOpts: func() *common.TLSOptions {
+				return &common.TLSOptions{
+					IssuerOptions: []common.IssuerOption{
+						withIssuerOfTypeCA("test-secret"),
+					},
+				}
+			},
+			validateFunc: func(t *testing.T, issuer *cmv1.Issuer) {
+				assert.NotNil(t, issuer.Spec.CA)
+				assert.Equal(t, "test-secret", issuer.Spec.CA.SecretName)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			opts := tt.setupOpts()
+			issuer := createIssuer(ns, tt.issuerName, opts)
+
+			assert.Equal(t, tt.issuerName, issuer.Name)
+			assert.Equal(t, ns, issuer.Namespace)
+			tt.validateFunc(t, issuer)
+		})
+	}
+}
+
+func TestCreateCertificate(t *testing.T) {
+	svc := commontest.LoadRuntimeFromFile(t, "vshnopenbao/deploy/01_default.yaml")
+
+	comp := &vshnv1.VSHNOpenBao{}
+	err := svc.GetObservedComposite(comp)
+	assert.NoError(t, err)
+
+	ns := comp.GetInstanceNamespace()
+
+	tests := []struct {
+		name         string
+		certName     string
+		setupOpts    func() *common.TLSOptions
+		validateFunc func(*testing.T, *cmv1.Certificate)
+	}{
+		{
+			name:     "basic certificate",
+			certName: "test-cert",
+			setupOpts: func() *common.TLSOptions {
+				return &common.TLSOptions{
+					CertOptions: []common.CertOptions{
+						withCertificateSecretName("test-secret"),
+					},
+				}
+			},
+			validateFunc: func(t *testing.T, cert *cmv1.Certificate) {
+				assert.Equal(t, "test-secret", cert.Spec.SecretName)
+				assert.Equal(t, cmv1.RSAKeyAlgorithm, cert.Spec.PrivateKey.Algorithm)
+				assert.Equal(t, cmv1.PKCS1, cert.Spec.PrivateKey.Encoding)
+				assert.Equal(t, 4096, cert.Spec.PrivateKey.Size)
+			},
+		},
+		{
+			name:     "CA certificate",
+			certName: "test-ca",
+			setupOpts: func() *common.TLSOptions {
+				return &common.TLSOptions{
+					CertOptions: []common.CertOptions{
+						withCertificateSecretName("test-ca-secret"),
+						withCertificateIsCA(true),
+						withCertificateDuration(time.Duration(87600 * time.Hour)),
+						withCertificateRenewBefore(time.Duration(2400 * time.Hour)),
+						withCertificateIssuerRef("selfsigned-issuer"),
+					},
+				}
+			},
+			validateFunc: func(t *testing.T, cert *cmv1.Certificate) {
+				assert.Equal(t, "test-ca-secret", cert.Spec.SecretName)
+				assert.True(t, cert.Spec.IsCA)
+				assert.Equal(t, time.Duration(87600*time.Hour), cert.Spec.Duration.Duration)
+				assert.Equal(t, time.Duration(2400*time.Hour), cert.Spec.RenewBefore.Duration)
+				assert.Equal(t, "selfsigned-issuer", cert.Spec.IssuerRef.Name)
+			},
+		},
+		{
+			name:     "server certificate with DNS names",
+			certName: "test-server",
+			setupOpts: func() *common.TLSOptions {
+				return &common.TLSOptions{
+					CertOptions: []common.CertOptions{
+						withCertificateSecretName("test-server-secret"),
+						withCertificateDNSName("service.namespace.svc.cluster.local"),
+						withCertificateDNSName("service.namespace.svc"),
+						withCertificateUsages([]cmv1.KeyUsage{"server auth", "client auth"}),
+						withCertificateIssuerRef("ca-issuer"),
+					},
+				}
+			},
+			validateFunc: func(t *testing.T, cert *cmv1.Certificate) {
+				assert.Equal(t, "test-server-secret", cert.Spec.SecretName)
+				assert.Contains(t, cert.Spec.DNSNames, "service.namespace.svc.cluster.local")
+				assert.Contains(t, cert.Spec.DNSNames, "service.namespace.svc")
+				assert.Contains(t, cert.Spec.Usages, cmv1.KeyUsage("server auth"))
+				assert.Contains(t, cert.Spec.Usages, cmv1.KeyUsage("client auth"))
+				assert.Equal(t, "ca-issuer", cert.Spec.IssuerRef.Name)
+				assert.Equal(t, "Issuer", cert.Spec.IssuerRef.Kind)
+				assert.Equal(t, "cert-manager.io", cert.Spec.IssuerRef.Group)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			opts := tt.setupOpts()
+			cert := createCertificate(ns, tt.certName, opts)
+
+			assert.Equal(t, tt.certName, cert.Name)
+			assert.Equal(t, ns, cert.Namespace)
+			assert.Equal(t, tt.certName+"-ca", cert.Spec.CommonName)
+			assert.Contains(t, cert.Spec.Subject.Organizations, "vshn-appcat-ca")
+			tt.validateFunc(t, cert)
+		})
+	}
+}
+
+func TestCertificateOptions(t *testing.T) {
+	cert := &cmv1.Certificate{
+		Spec: cmv1.CertificateSpec{},
+	}
+
+	// Test withCertificateSecretName
+	withCertificateSecretName("test-secret")(cert)
+	assert.Equal(t, "test-secret", cert.Spec.SecretName)
+
+	// Test withCertificateDNSName
+	withCertificateDNSName("test1.example.com")(cert)
+	withCertificateDNSName("test2.example.com")(cert)
+	assert.Len(t, cert.Spec.DNSNames, 2)
+	assert.Contains(t, cert.Spec.DNSNames, "test1.example.com")
+	assert.Contains(t, cert.Spec.DNSNames, "test2.example.com")
+
+	// Test withCertificateIsCA
+	withCertificateIsCA(true)(cert)
+	assert.True(t, cert.Spec.IsCA)
+
+	// Test withCertificateIssuerRef
+	withCertificateIssuerRef("test-issuer")(cert)
+	assert.Equal(t, "test-issuer", cert.Spec.IssuerRef.Name)
+	assert.Equal(t, "Issuer", cert.Spec.IssuerRef.Kind)
+	assert.Equal(t, "cert-manager.io", cert.Spec.IssuerRef.Group)
+
+	// Test withCertificateDuration
+	duration := time.Duration(87600 * time.Hour)
+	withCertificateDuration(duration)(cert)
+	assert.Equal(t, duration, cert.Spec.Duration.Duration)
+
+	// Test withCertificateRenewBefore
+	renewBefore := time.Duration(2400 * time.Hour)
+	withCertificateRenewBefore(renewBefore)(cert)
+	assert.Equal(t, renewBefore, cert.Spec.RenewBefore.Duration)
+
+	// Test withCertificateUsages
+	usages := []cmv1.KeyUsage{"server auth", "client auth"}
+	withCertificateUsages(usages)(cert)
+	assert.Equal(t, usages, cert.Spec.Usages)
+}
+
+func TestIssuerOptions(t *testing.T) {
+	// Test withIssuerOfTypeSelfSigned
+	issuer := &cmv1.Issuer{}
+	withIssuerOfTypeSelfSigned()(issuer)
+	assert.NotNil(t, issuer.Spec.SelfSigned)
+	assert.Equal(t, []string{}, issuer.Spec.SelfSigned.CRLDistributionPoints)
+
+	// Test withIssuerOfTypeCA
+	issuer2 := &cmv1.Issuer{}
+	withIssuerOfTypeCA("ca-secret")(issuer2)
+	assert.NotNil(t, issuer2.Spec.CA)
+	assert.Equal(t, "ca-secret", issuer2.Spec.CA.SecretName)
+}
+
+// Helper function to find connection detail by key
+func findConnectionDetail(details []xkube.ConnectionDetail, key string) *xkube.ConnectionDetail {
+	for _, detail := range details {
+		if detail.ToConnectionSecretKey == key {
+			return &detail
+		}
+	}
+	return nil
+}

14252dee9791f985dec7c96a84233e8aa85ec094
Gabriel Saratura <gabriel.saratura@vshn.net>
2026-01-08 17:31:56 +0100

    Update existing tests

--- pkg/controller/billing/delete_test.go
+++ pkg/controller/billing/delete_test.go
@@ -28,7 +28,9 @@ func TestShouldRemoveFinalizer(t *testing.T) {
 				Spec: vshnv1.BillingServiceSpec{
 					KeepAfterDeletion: 0,
 					Odoo: vshnv1.OdooSpec{
-						ProductID: "prod-123",
+						Items: []vshnv1.ItemSpec{
+							{ProductID: "prod-123", Value: "1", Unit: "instance"},
+						},
 					},
 				},
 			},
@@ -44,7 +46,9 @@ func TestShouldRemoveFinalizer(t *testing.T) {
 				Spec: vshnv1.BillingServiceSpec{
 					KeepAfterDeletion: 7,
 					Odoo: vshnv1.OdooSpec{
-						ProductID: "prod-123",
+						Items: []vshnv1.ItemSpec{
+							{ProductID: "prod-123", Value: "1", Unit: "instance"},
+						},
 					},
 				},
 			},
@@ -63,7 +67,9 @@ func TestShouldRemoveFinalizer(t *testing.T) {
 				Spec: vshnv1.BillingServiceSpec{
 					KeepAfterDeletion: 7,
 					Odoo: vshnv1.OdooSpec{
-						ProductID: "prod-123",
+						Items: []vshnv1.ItemSpec{
+							{ProductID: "prod-123", Value: "1", Unit: "instance"},
+						},
 					},
 				},
 			},
@@ -78,7 +84,9 @@ func TestShouldRemoveFinalizer(t *testing.T) {
 				},
 				Spec: vshnv1.BillingServiceSpec{
 					Odoo: vshnv1.OdooSpec{
-						ProductID: "prod-123",
+						Items: []vshnv1.ItemSpec{
+							{ProductID: "prod-123", Value: "1", Unit: "instance"},
+						},
 					},
 				},
 			},
@@ -94,7 +102,9 @@ func TestShouldRemoveFinalizer(t *testing.T) {
 				Spec: vshnv1.BillingServiceSpec{
 					KeepAfterDeletion: -1,
 					Odoo: vshnv1.OdooSpec{
-						ProductID: "prod-123",
+						Items: []vshnv1.ItemSpec{
+							{ProductID: "prod-123", Value: "1", Unit: "instance"},
+						},
 					},
 				},
 			},

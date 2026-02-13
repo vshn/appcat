1ea21b7b6bd30ee48c0ffd01f38c912499b6dfda
Mike Ditton <mike.ditton@vshn.net>
2026-02-13 15:04:36 +0100

    Fix maintenance schedule mutation and day rollover bugs
    This fixes mutation and maintenance scheduling on wrong days by
    replacing AddTime() with AddDuration() and moving offset logic into
    SetCustomMaintenanceSchedule

--- apis/vshn/v1/common_types_test.go
+++ apis/vshn/v1/common_types_test.go
@@ -42,40 +42,36 @@ func Test_GetTime(t *testing.T) {
 	}
 }
 
-func Test_AddTime(t *testing.T) {
+func Test_AddDuration_BasicCases(t *testing.T) {
 	tests := []struct {
-		name         string
-		scheduleSpec VSHNDBaaSMaintenanceScheduleSpec
-		d            time.Duration
-		want         string
+		name      string
+		timeOfDay TimeOfDay
+		d         time.Duration
+		want      string
 	}{
 		{
-			name: "GivenNormalSchedule_ThenExpectAddedTimeOfDay",
-			scheduleSpec: VSHNDBaaSMaintenanceScheduleSpec{
-				TimeOfDay: TimeOfDay("00:30:00"),
-			},
-			d:    60 * time.Minute,
-			want: "01:30:00",
+			name:      "GivenNormalSchedule_ThenExpectAddedTimeOfDay",
+			timeOfDay: TimeOfDay("00:30:00"),
+			d:         60 * time.Minute,
+			want:      "01:30:00",
 		},
 		{
-			name: "GivenEmptyTimeOfDay_ThenExpectAddedTimeOfDay",
-			scheduleSpec: VSHNDBaaSMaintenanceScheduleSpec{
-				TimeOfDay: TimeOfDay(""),
-			},
-			d:    10 * time.Minute,
-			want: "00:10:00",
+			name:      "GivenEmptyTimeOfDay_ThenExpectAddedTimeOfDay",
+			timeOfDay: TimeOfDay(""),
+			d:         10 * time.Minute,
+			want:      "00:10:00",
 		},
 		{
-			name:         "GivenNoTimeOfDay_ThenExpectAddedTimeOfDay",
-			scheduleSpec: VSHNDBaaSMaintenanceScheduleSpec{},
-			d:            5 * time.Minute,
-			want:         "00:05:00",
+			name:      "GivenNoTimeOfDay_ThenExpectAddedTimeOfDay",
+			timeOfDay: TimeOfDay(""),
+			d:         5 * time.Minute,
+			want:      "00:05:00",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			actualTime := tt.scheduleSpec.TimeOfDay.AddTime(tt.d)
-			assert.Equal(t, tt.want, string(actualTime))
+			resultTime, _ := tt.timeOfDay.AddDuration(tt.d)
+			assert.Equal(t, tt.want, string(resultTime))
 		})
 	}
 }
@@ -161,6 +157,123 @@ func Test_IsAppcatReleaseDisabled(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			assert.Equal(t, tt.want, tt.scheduleSpec.IsAppcatReleaseDisabled())
+		})
+	}
+}
+
+func Test_AddDuration(t *testing.T) {
+	tests := []struct {
+		name          string
+		timeOfDay     TimeOfDay
+		duration      time.Duration
+		wantTime      string
+		wantDayOffset int
+	}{
+		{
+			name:          "GivenMidnightRollover_ThenExpectNextDay",
+			timeOfDay:     TimeOfDay("23:50:00"),
+			duration:      20 * time.Minute,
+			wantTime:      "00:10:00",
+			wantDayOffset: 1,
+		},
+		{
+			name:          "GivenSameDay_ThenExpectNoDayOffset",
+			timeOfDay:     TimeOfDay("10:00:00"),
+			duration:      30 * time.Minute,
+			wantTime:      "10:30:00",
+			wantDayOffset: 0,
+		},
+		{
+			name:          "GivenExactMidnight_ThenExpectNoDayOffset",
+			timeOfDay:     TimeOfDay("23:00:00"),
+			duration:      1 * time.Hour,
+			wantTime:      "00:00:00",
+			wantDayOffset: 1,
+		},
+		{
+			name:          "GivenNegativeDuration_ThenExpectPreviousDay",
+			timeOfDay:     TimeOfDay("00:10:00"),
+			duration:      -20 * time.Minute,
+			wantTime:      "23:50:00",
+			wantDayOffset: -1,
+		},
+		{
+			name:          "GivenLargePositiveDuration_ThenExpectPositiveDayOffset",
+			timeOfDay:     TimeOfDay("10:00:00"),
+			duration:      15 * time.Hour,
+			wantTime:      "01:00:00",
+			wantDayOffset: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resultTime, dayOffset := tt.timeOfDay.AddDuration(tt.duration)
+			assert.Equal(t, tt.wantTime, string(resultTime), "Result time should match expected")
+			assert.Equal(t, tt.wantDayOffset, dayOffset, "Day offset should match expected")
+		})
+	}
+}
+
+func Test_AddDaysToWeekday(t *testing.T) {
+	tests := []struct {
+		name    string
+		weekday string
+		days    int
+		want    string
+	}{
+		{
+			name:    "GivenMondayPlus1_ThenExpectTuesday",
+			weekday: "monday",
+			days:    1,
+			want:    "tuesday",
+		},
+		{
+			name:    "GivenMondayPlus0_ThenExpectMonday",
+			weekday: "monday",
+			days:    0,
+			want:    "monday",
+		},
+		{
+			name:    "GivenSundayPlus1_ThenExpectMonday",
+			weekday: "sunday",
+			days:    1,
+			want:    "monday",
+		},
+		{
+			name:    "GivenMondayPlus7_ThenExpectMonday",
+			weekday: "monday",
+			days:    7,
+			want:    "monday",
+		},
+		{
+			name:    "GivenMondayMinus1_ThenExpectSunday",
+			weekday: "monday",
+			days:    -1,
+			want:    "sunday",
+		},
+		{
+			name:    "GivenSaturdayPlus2_ThenExpectMonday",
+			weekday: "saturday",
+			days:    2,
+			want:    "monday",
+		},
+		{
+			name:    "GivenInvalidWeekday_ThenExpectUnchanged",
+			weekday: "invalid",
+			days:    1,
+			want:    "invalid",
+		},
+		{
+			name:    "GivenWednesdayPlus10_ThenExpectSaturday",
+			weekday: "wednesday",
+			days:    10,
+			want:    "saturday",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := AddDaysToWeekday(tt.weekday, tt.days)
+			assert.Equal(t, tt.want, result)
 		})
 	}
 }
